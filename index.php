61fd595f223358b7947058653aa98cc03e6842b5
KeiyaMorii <kei8.m.0123@gmail.com>
2021-01-31 16:18:28 +0900

    論理積

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
-  echo 2 + 3 * 100 . "<br>";
-  echo (2 + 3) * 100 . "<br>";
+  $true = TRUE;
+  $false = FALSE;
+
+  $a = $true && $true;
+  $b = $true && $false;
+  $c = $true && $true && $true;
+  $d = $true && $false && $false;
+  $e = $true && ($true && $false); // ()内が優先
+  var_dump($a, $b, $c, $d, $e);
 ?>
