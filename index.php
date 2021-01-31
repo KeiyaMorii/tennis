50cab5b053577cd5a23ce63931fee1fcb8c8f76d
KeiyaMorii <kei8.m.0123@gmail.com>
2021-01-31 17:02:57 +0900

    for構文

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
-  $age = 23;
-  if ( $age < 20 ){
-    echo "購入できません";
-  } else if ( $age >= 20 && $age <=25 ){
-    echo "年齢確認が必要です";
-  } else {
-    echo "購入できます";
+  for ( $i = 1; $i <= 10; $i = $i + 1) {
+    echo $i . "<br>";
   }
 ?>
