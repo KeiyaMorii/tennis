854948d2485b5661574cdbf4814b997bbe5a57a0
KeiyaMorii <kei8.m.0123@gmail.com>
2021-01-31 16:24:37 +0900

    論理和

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
   $true = TRUE;
   $false = FALSE;
 
-  $a = $true && $true;
-  $b = $true && $false;
-  $c = $true && $true && $true;
-  $d = $true && $false && $false;
-  $e = $true && ($true && $false); // ()内が優先
-  var_dump($a, $b, $c, $d, $e);
+  $a = $true || $true;
+  $b = $true || $false;
+  $c = $true || $true || $true;
+  $d = $true || $false || $false;
+  $e = $true || ($true || $false);
+  $f = $false || $false;
+  var_dump($a, $b, $c, $d, $e, $f);
 ?>
