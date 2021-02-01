ae7dc40943f9288485240a5472e71fd837db95cd
KeiyaMorii <kei8.m.0123@gmail.com>
2021-02-01 19:31:23 +0900

    連想配列

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <pre>
 <?php
-  $friends = array("はるき", "かおる", "ひでと", "まさとし", "たかのり");
-  array_push($friends, "ともゆき");
-  var_dump($friends);
-
-  array_push($friends, "ゆうた", "てつや");
-  var_dump($friends);
+  $result = array(
+    "japanese" => 80,
+    "math"     => 75,
+    "science"  => 90,
+    "history"  => 85,
+    "english"  => 80
+  );
+  var_dump($result);
 ?>
 </pre>
