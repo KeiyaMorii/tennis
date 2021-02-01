fe9cf0beb28c847b1fe5d62a2c7c1d9313b18c4e
KeiyaMorii <kei8.m.0123@gmail.com>
2021-02-01 19:45:29 +0900

    値の追加

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <pre>
 <?php
-  $result["japanese"] = 80;
-  $result["math"]     = 75;
-  $result["science"]  = 90;
-  $result["history"]  = 85;
-  $result["english"]  = 80;
+  $result = array(
+    "japanese" => 80,
+    "math"     => 75,
+    "science"  => 90,
+    "history"  => 85,
+    "english"  => 80
+  );
+  var_dump($result);
+
+  $result["math"] = 85;
+  var_dump($result);
+
+  $result["music"] = 90;
   var_dump($result);
 ?>
 </pre>
