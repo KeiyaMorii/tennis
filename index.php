a3584e4f49fa56699524ef8f32f87f31844428f9
KeiyaMorii <kei8.m.0123@gmail.com>
2021-02-01 20:07:53 +0900

    for構文と配列

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <pre>
 <?php
-  $result = array("math" => 90, "english" => 80);
-  $friends = array(
-    "Haruki" => $result,
-  );
-  var_dump($friends);
-
-  $friends["Kaoru"] = array("math" => 95, "english" => 85);
-  var_dump($friends);
+  $friends = array("はるき", "かおる", "ひでと", "まさとし", "たかのり");
+  for ($i = 0; $i < 5; $i++) {
+    echo $friends[$i] . "<br>";
+  }
 ?>
 </pre>
